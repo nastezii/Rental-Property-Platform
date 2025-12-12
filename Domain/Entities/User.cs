da79c016f6ff31c95227c48af988cad2ebe74d18
nastezii <nastiashalak@gmail.com>
2025-12-12 13:16:11 +0600

    add universal User entity to represent owner, tenant, and reviewer

---
+++ Domain/Entities/User.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Entities.Base;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace Domain.Entities;
+
+public class User : TrackingEntity
+{
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string PasswordHash { get; set; } = string.Empty;
+
+    public List<Property> Properties { get; set; } = [];
+}
