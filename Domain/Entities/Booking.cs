7f83f489a2ad3afbda89f1b8415c9fe17916734b
nastezii <nastiashalak@gmail.com>
2025-12-12 13:20:43 +0600

    add Booking and Review entities for tenancy and ratings

---
+++ Domain/Entities/Booking.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Entities.Base;
+using System.Reflection;
+
+namespace Domain.Entities;
+
+public class Booking : TrackingEntity
+{
+    public int TenantId { get; set; }
+    public int ListingId { get; set; }
+    public DateTime StartDate { get; set; }
+    public DateTime EndDate { get; set; }
+
+    public User Tenant { get; set; } = null!;
+}
---
+++ Domain/Entities/Review.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Entities.Base;
+
+namespace Domain.Entities;
+
+public class Review : TrackingEntity
+{
+    public int ReviewerId { get; set; }
+    public int PropertyId { get; set; }
+    public int Rating { get; set; }
+    public string Comment { get; set; } = string.Empty;
+
+    public User Reviewer { get; set; } = null!;
+}
