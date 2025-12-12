831477d7b3545a8c1db56eafbfe412fa75055325
nastezii <nastiashalak@gmail.com>
2025-12-12 21:16:52 +0600

    add Amenity entity configuration with unique name, constraints, relationships, and row version concurrency

---
+++ Infrastructure/Configurations/AmenityConfiguration.cs
@@ -0,0 +1,39 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Configurations;
+
+public class AmenityConfiguration : IEntityTypeConfiguration<Amenity>
+{
+    public void Configure(EntityTypeBuilder<Amenity> builder)
+    {
+        builder.ToTable("amenities");
+        builder.HasKey(a => a.Id);
+        builder.HasIndex(a => a.Name).IsUnique();
+
+        builder.Property(x => x.RowVersion)
+            .IsRowVersion()
+            .IsConcurrencyToken()
+            .HasDefaultValueSql("gen_random_bytes(8)");
+
+        builder.Property(a => a.Name)
+            .IsRequired()
+            .HasMaxLength(100);
+
+        builder.Property(a => a.Description)
+            .IsRequired()
+            .HasMaxLength(500);
+
+        builder.ToTable(t =>
+        {
+            t.HasCheckConstraint("chk_amenities_name_length", "LENGTH(\"name\") >= 2");
+            t.HasCheckConstraint("chk_amenities_description_length", "LENGTH(\"description\") >= 5");
+        });
+
+        builder.HasMany(a => a.PropertyAmenities)
+            .WithOne(pa => pa.Amenity)
+            .HasForeignKey(pa => pa.AmenityId)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}
