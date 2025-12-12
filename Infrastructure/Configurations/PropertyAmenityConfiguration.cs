a7b6e47abd788a9e0911546b75485a855dd4e663
nastezii <nastiashalak@gmail.com>
2025-12-12 20:50:58 +0600

    add PropertyAmenity entity configuration with unique index, relationships, and row version concurrency

---
+++ Infrastructure/Configurations/PropertyAmenityConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Configurations;
+
+public class PropertyAmenityConfiguration : IEntityTypeConfiguration<PropertyAmenity>
+{
+    public void Configure(EntityTypeBuilder<PropertyAmenity> builder)
+    {
+        builder.ToTable("property_amenities");
+        builder.HasKey(pa => pa.Id);
+        builder.HasIndex(pa => new { pa.PropertyId, pa.AmenityId }).IsUnique();
+
+        builder.Property(x => x.RowVersion)
+            .IsRowVersion()
+            .IsConcurrencyToken()
+            .HasDefaultValueSql("gen_random_bytes(8)");
+
+        builder.HasOne(pa => pa.Property)
+            .WithMany(p => p.PropertyAmenities)
+            .HasForeignKey(pa => pa.PropertyId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        builder.HasOne(pa => pa.Amenity)
+            .WithMany(a => a.PropertyAmenities)
+            .HasForeignKey(pa => pa.AmenityId)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}
