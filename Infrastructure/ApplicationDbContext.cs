1235cc9d2296756f4a70a91398b77fe416e88b06
nastezii <nastiashalak@gmail.com>
2025-12-13 13:01:06 +0600

    configure database access layer

---
+++ Infrastructure/ApplicationDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Domain.Entities;
+using Infrastructure.Configurations;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure;
+
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
+{
+    public DbSet<User> Users { get; set; }
+    public DbSet<Property> Properties { get; set; }
+    public DbSet<Amenity> Amenities { get; set; }
+    public DbSet<PropertyAmenity> PropertyAmenities { get; set; }
+    public DbSet<Listing> Listings { get; set; }
+    public DbSet<Booking> Bookings { get; set; }
+    public DbSet<Review> Reviews { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfiguration(new UserConfiguration());
+        modelBuilder.ApplyConfiguration(new PropertyConfiguration());
+        modelBuilder.ApplyConfiguration(new AmenityConfiguration());
+        modelBuilder.ApplyConfiguration(new PropertyAmenityConfiguration());
+        modelBuilder.ApplyConfiguration(new ListingConfiguration());
+        modelBuilder.ApplyConfiguration(new BookingConfiguration());
+        modelBuilder.ApplyConfiguration(new ReviewConfiguration());
+
+        base.OnModelCreating(modelBuilder);
+    }
+}
