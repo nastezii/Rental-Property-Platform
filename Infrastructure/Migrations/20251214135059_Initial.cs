e97adbfca568dafad304329ef7f636220a13f5f6
nastezii <nastiashalak@gmail.com>
2025-12-14 13:53:08 +0600

    initial database schema with tables, keys, and constraints

---
+++ Infrastructure/Migrations/20251214135059_Initial.cs
@@ -0,0 +1,296 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("CREATE EXTENSION IF NOT EXISTS pgcrypto;");
+
+            migrationBuilder.CreateTable(
+                name: "amenities",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    name = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    description = table.Column<string>(type: "character varying(500)", maxLength: 500, nullable: false),
+                    row_version = table.Column<byte[]>(type: "bytea", rowVersion: true, nullable: false, defaultValueSql: "gen_random_bytes(8)")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_amenities", x => x.id);
+                    table.CheckConstraint("chk_amenities_description_length", "LENGTH(\"description\") >= 5");
+                    table.CheckConstraint("chk_amenities_name_length", "LENGTH(\"name\") >= 2");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "users",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    first_name = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    last_name = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    email = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    password_hash = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false),
+                    row_version = table.Column<byte[]>(type: "bytea", rowVersion: true, nullable: false, defaultValueSql: "gen_random_bytes(8)"),
+                    date_added = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_users", x => x.id);
+                    table.CheckConstraint("chk_users_email_length", "LENGTH(\"email\") >= 7");
+                    table.CheckConstraint("chk_users_first_name_length", "LENGTH(\"first_name\") >= 2");
+                    table.CheckConstraint("chk_users_last_name_length", "LENGTH(\"last_name\") >= 2");
+                    table.CheckConstraint("chk_users_password_hash_length", "LENGTH(\"password_hash\") >= 60");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "properties",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    user_id = table.Column<int>(type: "integer", nullable: false),
+                    title = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false),
+                    description = table.Column<string>(type: "text", nullable: false),
+                    location = table.Column<string>(type: "character varying(500)", maxLength: 500, nullable: false),
+                    property_type = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    row_version = table.Column<byte[]>(type: "bytea", rowVersion: true, nullable: false, defaultValueSql: "gen_random_bytes(8)")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_properties", x => x.id);
+                    table.CheckConstraint("chk_properties_description_length", "LENGTH(\"description\") >= 10");
+                    table.CheckConstraint("chk_properties_location_length", "LENGTH(\"location\") >= 5");
+                    table.CheckConstraint("chk_properties_title_length", "LENGTH(\"title\") >= 5");
+                    table.ForeignKey(
+                        name: "fk_properties_users_user_id",
+                        column: x => x.user_id,
+                        principalTable: "users",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "listings",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    property_id = table.Column<int>(type: "integer", nullable: false),
+                    price = table.Column<decimal>(type: "numeric(10,2)", precision: 10, scale: 2, nullable: false),
+                    currency = table.Column<string>(type: "character varying(3)", maxLength: 3, nullable: false),
+                    min_rental_period_in_months = table.Column<int>(type: "integer", nullable: false),
+                    status = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    row_version = table.Column<byte[]>(type: "bytea", rowVersion: true, nullable: false, defaultValueSql: "gen_random_bytes(8)"),
+                    date_added = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_listings", x => x.id);
+                    table.CheckConstraint("chk_listings_min_period_positive", "\"min_rental_period_in_months\" > 0");
+                    table.CheckConstraint("chk_listings_price_positive", "\"price\" > 0");
+                    table.ForeignKey(
+                        name: "fk_listings_properties_property_id",
+                        column: x => x.property_id,
+                        principalTable: "properties",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "property_amenities",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    property_id = table.Column<int>(type: "integer", nullable: false),
+                    amenity_id = table.Column<int>(type: "integer", nullable: false),
+                    row_version = table.Column<byte[]>(type: "bytea", rowVersion: true, nullable: false, defaultValueSql: "gen_random_bytes(8)")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_property_amenities", x => x.id);
+                    table.ForeignKey(
+                        name: "fk_property_amenities_amenities_amenity_id",
+                        column: x => x.amenity_id,
+                        principalTable: "amenities",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "fk_property_amenities_properties_property_id",
+                        column: x => x.property_id,
+                        principalTable: "properties",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "reviews",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    reviewer_id = table.Column<int>(type: "integer", nullable: false),
+                    property_id = table.Column<int>(type: "integer", nullable: false),
+                    rating = table.Column<int>(type: "integer", nullable: false),
+                    comment = table.Column<string>(type: "text", nullable: false),
+                    row_version = table.Column<byte[]>(type: "bytea", rowVersion: true, nullable: false, defaultValueSql: "gen_random_bytes(8)"),
+                    date_added = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_reviews", x => x.id);
+                    table.CheckConstraint("chk_reviews_comment_length", "LENGTH(\"comment\") >= 10");
+                    table.CheckConstraint("chk_reviews_rating_range", "\"rating\" >= 1 AND \"rating\" <= 5");
+                    table.ForeignKey(
+                        name: "fk_reviews_properties_property_id",
+                        column: x => x.property_id,
+                        principalTable: "properties",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "fk_reviews_users_reviewer_id",
+                        column: x => x.reviewer_id,
+                        principalTable: "users",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "bookings",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    tenant_id = table.Column<int>(type: "integer", nullable: false),
+                    listing_id = table.Column<int>(type: "integer", nullable: false),
+                    start_date = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    end_date = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    row_version = table.Column<byte[]>(type: "bytea", rowVersion: true, nullable: false, defaultValueSql: "gen_random_bytes(8)"),
+                    date_added = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_bookings", x => x.id);
+                    table.CheckConstraint("chk_bookings_end_after_start", "\"end_date\" > \"start_date\"");
+                    table.ForeignKey(
+                        name: "fk_bookings_listings_listing_id",
+                        column: x => x.listing_id,
+                        principalTable: "listings",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "fk_bookings_users_tenant_id",
+                        column: x => x.tenant_id,
+                        principalTable: "users",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "ix_amenities_name",
+                table: "amenities",
+                column: "name",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "ix_bookings_listing_id",
+                table: "bookings",
+                column: "listing_id");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_bookings_start_date_end_date",
+                table: "bookings",
+                columns: new[] { "start_date", "end_date" });
+
+            migrationBuilder.CreateIndex(
+                name: "ix_bookings_tenant_id",
+                table: "bookings",
+                column: "tenant_id");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_listings_property_id",
+                table: "listings",
+                column: "property_id");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_listings_status_price",
+                table: "listings",
+                columns: new[] { "status", "price" });
+
+            migrationBuilder.CreateIndex(
+                name: "ix_properties_location",
+                table: "properties",
+                column: "location");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_properties_user_id",
+                table: "properties",
+                column: "user_id");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_property_amenities_amenity_id",
+                table: "property_amenities",
+                column: "amenity_id");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_property_amenities_property_id_amenity_id",
+                table: "property_amenities",
+                columns: new[] { "property_id", "amenity_id" },
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "ix_reviews_property_id",
+                table: "reviews",
+                column: "property_id");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_reviews_rating",
+                table: "reviews",
+                column: "rating");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_reviews_reviewer_id",
+                table: "reviews",
+                column: "reviewer_id");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_users_email",
+                table: "users",
+                column: "email",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "property_amenities");
+
+            migrationBuilder.DropTable(
+                name: "reviews");
+
+            migrationBuilder.DropTable(
+                name: "listings");
+
+            migrationBuilder.DropTable(
+                name: "amenities");
+
+            migrationBuilder.DropTable(
+                name: "properties");
+
+            migrationBuilder.DropTable(
+                name: "users");
+        }
+    }
+}
