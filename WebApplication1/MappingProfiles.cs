f9246664daf6c75d435bc1d3ea757a3670f5ff77
nastezii <nastiashalak@gmail.com>
2025-12-15 14:11:05 +0600

    map listing, property and user entities to API DTOs

---
+++ WebApplication1/MappingProfiles.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using Domain.Dtos.CreateNewUserListingDto;
+using Domain.Dtos.General;
+using Domain.Entities;
+
+namespace WebApplication1;
+
+public class MappingProfiles : Profile
+{
+    public MappingProfiles()
+    {
+        CreateMap<UserRequestDto, User>();
+        CreateMap<User, UserResponseDto>();
+        
+        CreateMap<PropertyRequestDto, Property>();
+        CreateMap<ListingRequestDto, Listing>();
+
+        CreateMap<Property, PropertyResponseDto>()
+            .ForMember(dest => dest.Amenities, opt => opt.MapFrom(src => src.PropertyAmenities.Select(pa => pa.Amenity)))
+            .ForMember(dest => dest.Rating, 
+                opt => opt.MapFrom(src =>
+                    src.Reviews.Any()
+                        ? src.Reviews.Average(r => r.Rating)
+                        : 0
+                ));
+
+        CreateMap<Amenity, AmenityResponseDto>();
+        CreateMap<Listing, ListingResponseDto>();
+    }
+}
