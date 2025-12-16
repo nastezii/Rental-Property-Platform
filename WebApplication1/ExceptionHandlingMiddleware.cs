23c297bc763bf0eb7447cb62fe6ed77b93a1a142
nastezii <nastiashalak@gmail.com>
2025-12-16 12:37:45 +0600

    add global exception handling middleware

---
+++ WebApplication1/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Npgsql;
+
+namespace WebApplication1;
+
+public class ExceptionHandlingMiddleware(RequestDelegate next, ILogger<ExceptionHandlingMiddleware> logger)
+{
+    public async Task Invoke(HttpContext context)
+    {
+        try
+        {
+            await next(context);
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex, "An error occured");
+            await HandleExceptionAsync(context, ex);
+        }
+    }
+
+    private static Task HandleExceptionAsync(HttpContext context, Exception exception)
+    {
+        var pgEx = (exception as DbUpdateException)?.InnerException as PostgresException;
+
+        var response = new 
+        {
+            error = pgEx?.MessageText ?? exception.Message,
+            trace = pgEx is null ? exception.StackTrace : null
+        };
+
+        context.Response.ContentType = "application/json";
+        context.Response.StatusCode = StatusCodes.Status400BadRequest;
+
+        return context.Response.WriteAsJsonAsync(response);
+    }
+}
